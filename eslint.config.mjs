e693bec6d9855e02e7c86c5f2287eec719d45e83
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-08-21 15:08:34 +0100

    PROJ-2574: Update eslint config setup - update rule, add lint scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+// import pluginReact from "eslint-plugin-react";
 
 
 export default [
@@ -9,5 +9,11 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+      rules: {
+          "no-unused-vars": "warn",
+          "no-undef": "warn"
+      }
+  }
+  // pluginReact.configs.flat.recommended,
 ];
