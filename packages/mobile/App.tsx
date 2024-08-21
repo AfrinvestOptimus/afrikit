0d743069740e74664982af1cd83cb65f84b4f720
Amire Steve <zoe_steve@hotmail.com>
2024-08-21 23:01:06 +0100

    PROJ-2478-React-Native-Setup: Add dark mode toggle and additional dependencies #2478
    This commit includes the integration of a color scheme toggle in the App component. It also introduces new dependencies including "@gorhom/bottom-sheet", "react-native-svg", "react-native-remix-icon", and "react-native-gesture-handler". The user interface style in the configuration is set to automatic to support both light and dark modes.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,13 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import "./global.css";
+import { useColorScheme } from "nativewind";
+import Icon from "react-native-remix-icon";
+import colors from "../shared/colors";
 
 export default function App() {
+  const { colorScheme, setColorScheme } = useColorScheme();
   return (
-    <View className={"justify-center items-center flex-1 bg-dark-optiblue4"}>
-      <Text className={"text-dark-optiblue9"}>
+    <View
+      className={
+        "justify-center items-center flex-1 bg-light-optiblue4 dark:bg-dark-optiblue4"
+      }
+    >
+      <Text className={"text-light-optiblue9 dark:text-dark-optiblue11"}>
         Open up App.tsx to start working on your app!
       </Text>
+
+      <Icon
+        name="ri-bank-fill"
+        size="24"
+        color={colorScheme === "light" ? colors.light.cyan9 : colors.dark.cyan9}
+      />
+
+      <Pressable
+        onPress={() =>
+          setColorScheme(colorScheme === "light" ? "dark" : "light")
+        }
+        className={"bg-light-optiblue9 px-md py-sm rounded-lg mt-2xl"}
+      >
+        <Text className="">Set Colorscheme</Text>
+      </Pressable>
       <StatusBar style="light" />
     </View>
   );
