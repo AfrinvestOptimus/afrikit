ece593f953bdb2af1ab53b7adf5370e8b1762484
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-08-22 10:31:42 +0100

    PROJ-2574: Update prettier and eslint rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,12 @@
-module.exports = {
+export default {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
-  "semi": false
+  "semi": false,
+  tabWidth: 2,
+  printWidth: 100,
 
   // bracketSpacing: true,
   // singleQuote: true,
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 // import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-      rules: {
-          "no-unused-vars": "warn",
-          "no-undef": "warn"
-      }
-  }
   // pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'], // Change from 'error' to 'warn'
+    },
+  },
 ];
