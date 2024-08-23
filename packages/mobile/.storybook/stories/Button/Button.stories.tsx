d1bfb1b6506daea7d60cc9bf45e43fb1d58db352
Nkechi <kech3443@gmail.com>
2024-08-23 16:05:04 +0100

    PROJ-2575 Updated configuration to make Storybook functional

--- packages/mobile/.storybook/stories/Button/Button.stories.tsx
+++ packages/mobile/.storybook/stories/Button/Button.stories.tsx
@@ -3,21 +3,21 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { MyButton } from './Button';
 
 const MyButtonMeta: Meta<typeof MyButton> = {
-  title: 'MyButton',
-  component: MyButton,
-  argTypes: {
-    onPress: { action: 'pressed the button' },
-  },
-  args: {
-    text: 'Hello world',
-  },
-  decorators: [
-    (Story) => (
-      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-        <Story />
-      </View>
-    ),
-  ],
+	title: 'MyButton',
+	component: MyButton,
+	argTypes: {
+		onPress: { action: 'pressed the button' },
+	},
+	args: {
+		text: 'Hello world',
+	},
+	decorators: [
+		(Story) => (
+			<View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+				<Story />
+			</View>
+		),
+	],
 };
 
 export default MyButtonMeta;
@@ -25,7 +25,7 @@ export default MyButtonMeta;
 export const Basic: StoryObj<typeof MyButton> = {};
 
 export const AnotherExample: StoryObj<typeof MyButton> = {
-  args: {
-    text: 'Another example',
-  },
+	args: {
+		text: 'Another example',
+	},
 };
--- packages/mobile/.storybook/stories/Button/Button.tsx
+++ packages/mobile/.storybook/stories/Button/Button.tsx
@@ -1,24 +1,28 @@
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export type MyButtonProps = {
-  onPress: () => void;
-  text: string;
+export type ButtonProps = {
+	onPress: () => void;
+	text?: string; // Make the prop optional
 };
 
-export const MyButton = ({ onPress, text }: MyButtonProps) => {
-  return (
-    <TouchableOpacity style={styles.container} onPress={onPress} activeOpacity={0.8}>
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
-  );
+export const MyButton = ({ onPress, text = 'Default Text' }: ButtonProps) => {
+	return (
+		<TouchableOpacity
+			style={styles.container}
+			onPress={onPress}
+			activeOpacity={0.8}
+		>
+			<Text style={styles.text}>{text}</Text>
+		</TouchableOpacity>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    backgroundColor: 'purple',
-    borderRadius: 8,
-  },
-  text: { color: 'white' },
+	container: {
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+		backgroundColor: 'purple',
+		borderRadius: 8,
+	},
+	text: { color: 'white' },
 });
