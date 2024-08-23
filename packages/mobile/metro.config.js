ac8e63ad8dd4a7f9c91e67d234ab7d51742f154a
kyeltech <kyel2030@gmail.com>
2024-08-23 10:44:09 +0100

    PROJ-2592: Implement Storybook

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,6 +1,14 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+// const { generate } = require('@storybook/react-native/scripts/generate');
+
+
+// generate({
+//     configPath: path.resolve(__dirname, './.storybook'),
+//   });
 
 const config = getDefaultConfig(__dirname);
+
+// config.transformer.unstable_allowRequireContext = true;
 
 module.exports = withNativeWind(config, { input: "./global.css" });
