0bd3c8d22c78d861f00940a6ebae66b046077498
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-08-24 03:46:08 +0100

    PROJ-2553: Implement the logic for customizing the AppToast component

--- packages/mobile/molecules/app-toast.tsx
+++ packages/mobile/molecules/app-toast.tsx
@@ -19,11 +19,50 @@ export const showSuccessMessage = (message: string = 'Success') => {
     duration: 4000,
   })
 }
+
+const dictionary = {
+  type: {
+    success: 'success',
+    error: 'danger',
+    neutral: 'default',
+  },
+  icon: {
+    success: 'checkbox-circle-fill',
+    error: 'error-warning-fill',
+    neutral: 'checkbox-circle-fill',
+  },
+  color: {
+    success: colors.dark.background.success.base.DEFAULT,
+    error: colors.dark.background.error.base.DEFAULT,
+    neutral: colors.dark['contrast-black'],
+  },
+}
 const transformType = {
   success: 'success',
   error: 'danger',
   neutral: 'default',
-} 
+}
+type AppToastMessageOptions = {
+  type: 'success' | 'error' | 'neutral'
+  showIcon?: boolean
+  icon?: string
+  message: string
+}
+export const showToastMessage = ({
+  type,
+  icon,
+  showIcon = true,
+  message,
+}: AppToastMessageOptions) => {
+  const messageType = transformType[type] || transformType.neutral
+  const iconName = dictionary.icon[type] || 'checkbox-circle-fill'
+  showMessage({
+    message,
+    type: messageType as MessageType,
+    backgroundColor: dictionary.color[type],
+    duration: 2000,
+  })
+}
 
 type AppToastBase = {
   position: Position
