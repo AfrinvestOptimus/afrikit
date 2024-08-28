91ef1500eb05218ca2aed51cd334d98db9327432
Amire Steve <zoe_steve@hotmail.com>
2024-08-28 23:26:08 +0100

    PROJ-2504-AppTitle: Add AppTitle component and update type definitions
    This commit introduces the AppTitle component for title rendering in the application. It includes defining AppTitleAtomProps in types and the actual implementation in AppTitle.tsx. The new types and component ensure better structure and clarity for titles.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/mobile/types/atoms.d.ts
+++ packages/mobile/types/atoms.d.ts
@@ -15,15 +15,27 @@ interface AppTextOwnProps {
   children: string | ReactNode;
 }
 
-type AppTextAtomProps = AppTextOwnProps &
+export type AppTextAtomProps = AppTextOwnProps &
   Omit<TextProps, keyof AppTextOwnProps>;
 
-/*export interface AppTextAtomProps extends TextProps {
-  size?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-  color?: TailwindColorKey;
-  trim?: "normal" | "start" | "end" | "both";
-  weight?: "light" | "normal" | "medium" | "bold";
-  highContrast?: boolean;
-  align?: "left" | "center" | "right";
-  children: string | ReactNode;
-}*/
+export type AppTitleAtomProps = {
+  title: string;
+  align: "left" | "center";
+  hasSubtitle: boolean;
+  subtitle?: string;
+  titlePosition?: "top" | "bottom";
+  spacing?: 1 | 2 | 3;
+} & (
+  | {
+      hasSubtitle: false;
+      subtitle: never;
+      titlePosition: never;
+      spacing: never;
+    }
+  | {
+      hasSubtitle: true;
+      subtitle: string;
+      titlePosition?: "top" | "bottom";
+      spacing?: 1 | 2 | 3;
+    }
+);
