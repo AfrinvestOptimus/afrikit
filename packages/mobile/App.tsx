9363cdc52aa321da20ad3731343be646b0855130
Amire Steve <zoe_steve@hotmail.com>
2024-08-30 23:35:29 +0100

    PROJ-2555-app-text: Update yarn.lock and refactor AppText component
    Added "tailwind-variants" dependency and corresponding updates to yarn.lock. Refactored AppText component to simplify class name logic using `tailwind-variants` and improved code consistency.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,53 +1,58 @@
-import { StatusBar } from 'expo-status-bar'
-import { Appearance, View } from 'react-native'
-import './global.css'
-import StorybookUIRoot from './.storybook'
-import { useColorScheme } from 'nativewind'
+import { StatusBar } from "expo-status-bar";
+import { Appearance, View } from "react-native";
+import "./global.css";
+import StorybookUIRoot from "./.storybook";
+import { useColorScheme } from "nativewind";
 import {
   Manrope_400Regular,
   Manrope_500Medium,
   Manrope_600SemiBold,
   Manrope_700Bold,
   useFonts,
-} from '@expo-google-fonts/manrope'
+} from "@expo-google-fonts/manrope";
 
-import AppTitle from './atoms/AppTitle'
-import AppText from './atoms/AppText'
+import AppTitle from "./atoms/AppTitle";
+import AppText from "./atoms/AppText";
 
 export default function App() {
-  const { colorScheme } = useColorScheme()
-  const { setColorScheme } = Appearance
+  const { colorScheme } = useColorScheme();
+  const { setColorScheme } = Appearance;
   const [fontsLoaded, fontError] = useFonts({
     Manrope400: Manrope_400Regular,
     Manrope500: Manrope_500Medium,
     Manrope600: Manrope_600SemiBold,
     Manrope700: Manrope_700Bold,
-  })
+  });
 
   if (!fontsLoaded || fontError) {
-    return null
+    return null;
   }
 
-  const SHOW_STORYBOOK = true
+  const SHOW_STORYBOOK = true;
   if (SHOW_STORYBOOK) {
-    return <StorybookUIRoot />
+    return <StorybookUIRoot />;
   }
 
   return (
-    <View className={'justify-center flex-1 bg-light-optiblue4 dark:bg-dark-optiblue4 px-2xl'}>
+    <View
+      className={
+        "justify-center flex-1 bg-light-optiblue4 dark:bg-dark-optiblue4 px-2xl"
+      }
+    >
       <AppText
-        size={3}
-        color={'text-dark-orange9'}
-        weight={'medium'}
-        align={'left'}
-        className={'mb-2xl'}
-        onPress={() => console.log('AppText pressed')}>
-        Small bold text
+        size={2}
+        color={"text-dark-red9"}
+        weight={"regular"}
+        align={"left"}
+        className={"mb-2xl"}
+        onPress={() => console.log("AppText pressed")}
+      >
+        Small bold text involved
       </AppText>
 
       <AppTitle
-        title={'Title'}
-        align={'left'}
+        title={"Title"}
+        align={"left"}
         hasSubtitle={true}
         subtitle="Subtitle"
         spacing={1}
@@ -56,5 +61,5 @@ export default function App() {
 
       <StatusBar style="dark" backgroundColor="red" />
     </View>
-  )
+  );
 }
--- packages/mobile/atoms/AppText.tsx
+++ packages/mobile/atoms/AppText.tsx
@@ -1,86 +1,65 @@
-import { Text } from 'react-native'
-import { AppTextAtomProps } from '../types/atoms'
-import { useColorScheme } from 'nativewind'
-import classNames from '../utilities/classnames'
+import { Text } from "react-native";
+import { AppTextAtomProps } from "../types/atoms";
+import { useColorScheme } from "nativewind";
+import { tv } from "tailwind-variants";
 
-const sizeClasses = {
-  1: 'xs',
-  2: 'sm',
-  3: 'base',
-  4: 'lg',
-  5: 'xl',
-  6: '2xl',
-  7: '3xl',
-  8: '4xl',
-  9: '5xl',
-}
-const alignmentClasses = {
-  left: 'text-left',
-  center: 'text-center',
-  right: 'text-right',
-}
+const textVariants = tv({
+  base: "text-left",
+  variants: {
+    size: {
+      1: "text-xs leading-xs",
+      2: "text-sm leading-sm",
+      3: "text-base leading-base",
+      4: "text-lg leading-lg",
+      5: "text-xl leading-xl",
+      6: "text-2xl leading-2xl",
+      7: "text-3xl leading-3xl",
+      8: "text-4xl leading-4xl",
+      9: "text-5xl leading-5xl",
+    },
+    weight: {
+      regular: "font-regular",
+      medium: "font-medium",
+      semibold: "font-semibold",
+      bold: "font-bold",
+    },
+    align: {
+      left: "text-left",
+      center: "text-center",
+      right: "text-right",
+    },
+  },
+});
 
-const weightClasses = {
-  regular: 'body',
-  medium: 'title',
-  semibold: 'head',
-  bold: 'bold',
-}
-
-/**
- * React functional component for rendering text with customizable properties.
- *
- * @component AppText
- * @param {Object} props - The properties for customizing the text.
- * @param {number} props.size - Select the size of the text from 1-9. The default value is 3.
- * @param {string} props.color - Select the color of the text from the Tailwind color palette. The default value is
- *   black.
- * @param {string} props.trim - Select the trim of the text from normal, start, end, or both. The default value is
- *   normal.
- * @param {string} props.weight - Select the weight of the text from light, normal, medium, or bold. The default value
- *   is light.
- * @param {boolean} props.highContrast - Select the high contrast of the text. The default value is false.
- * @param {string} props.align - Select the alignment of the text from left, center, or right. The default value is
- *   left.
- * @param {ReactNode} props.children - The child elements to be rendered inside the text component.
- * @param {Object} props.rest - The rest of the props to be passed to the containing Text component.
- * @returns {ReactElement} The rendered AppText component.
- */
 const AppText = ({
   size = 3,
-  color = 'text-dark-slate4',
-  trim = 'normal',
-  weight = 'regular',
+  color = "text-dark-slate4",
+  trim = "normal",
+  weight = "regular",
   highContrast = false,
-  align = 'left',
+  align = "left",
   children,
   className,
   ...rest
 }: AppTextAtomProps) => {
-  const { colorScheme } = useColorScheme()
+  const { colorScheme } = useColorScheme();
 
-  const textSize = sizeClasses[size] || 'base'
-  const textWeight = weightClasses[weight] || 'body'
+  const highContrastClass = highContrast
+    ? `text-${colorScheme}-type-gray-muted`
+    : "";
 
-  const finalClassName = classNames(
-    alignmentClasses[align] || '',
-    color || '',
-    `text-${textSize}-${textWeight}`,
-    highContrast ? `text-${colorScheme}-type-gray-muted` : '',
-    className || '',
-  )
-
-  console.log('finalClassName', finalClassName)
+  const variantClasses = textVariants({ size, weight, align } as any);
 
   return (
     <Text
-      className={`
-        ${finalClassName}
-      `}
-      {...rest}>
+      className={`${variantClasses} ${color} ${highContrastClass} ${
+        className || ""
+      }`}
+      {...rest}
+    >
       {children}
     </Text>
-  )
-}
+  );
+};
 
-export default AppText
+export default AppText;
