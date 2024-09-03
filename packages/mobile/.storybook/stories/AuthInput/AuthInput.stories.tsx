e5284b8ad10ab669c501c876b9e2780b8914477f
Olashile-A <adegbiteolashile@ymail.com>
2024-09-03 09:12:13 +0100

    PROJ-2606: create authInput storybook

---
+++ packages/mobile/.storybook/stories/AuthInput/AuthInput.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { View } from 'react-native'
+import AuthInput from '../../../molecules/AuthInput'
+
+const AuthInputMeta: Meta<typeof AuthInput> = {
+  title: 'AuthInput',
+  component: AuthInput,
+  argTypes: {
+    count: {
+      control: 'select',
+      options: [1, 2, 3, 4, 5, 6],
+    },
+    isError: {
+      control: 'boolean',
+    },
+    keypad: {
+      control: 'select',
+      options: ['Custom', 'Native'],
+    },
+    onValueChange: {
+      action: 'pressed the button',
+    },
+    errorMessage: {
+      control: 'text',
+    },
+  },
+  decorators: [
+    Story => (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Story />
+      </View>
+    ),
+  ],
+}
+
+export default AuthInputMeta
+
+type Story = StoryObj<typeof AuthInputMeta>
+
+export const Basic: Story = {
+  args: {
+    count: 4,
+    isError: false,
+    keypad: 'Native',
+    errorMessage: '',
+  },
+}
+
+export const CustomKeypad: Story = {
+  args: {
+    count: 4,
+    isError: false,
+    keypad: 'Custom',
+    errorMessage: '',
+  },
+}
+
+export const ErrorMessage: Story = {
+  args: {
+    count: 4,
+    isError: true,
+    keypad: 'Native',
+    errorMessage: 'Mismatch',
+  },
+}
