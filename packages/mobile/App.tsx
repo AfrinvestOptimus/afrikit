bbc43665e239f4cd8a288f62f565a89011955bd9
Nkechi <kech3443@gmail.com>
2024-09-04 08:45:00 +0100

    PROJ-2491 Design component for AppModalLoader

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -11,19 +11,16 @@ import { AppTopBar } from './molecules/AppTopBar'
 import { StatusBar } from 'expo-status-bar'
 import { useColorScheme } from 'nativewind'
 import { Controller, useForm } from 'react-hook-form'
-import { Alert, Appearance, View } from 'react-native'
+import { Alert, Appearance, Button, View } from 'react-native'
 import StorybookUIRoot from './.storybook'
 import './global.css'
- 
-import { AppTopBar } from './molecules/AppTopBar'
 import { FormData } from './types/atoms'
 
+import { useState } from 'react'
 import AppText from './atoms/AppText'
 import AppTitle from './atoms/AppTitle'
-import AppInput from './molecules/AppInput'
 import { AppModalLoader } from './molecules/AppModalLoader'
 import AppPasswordInput from './molecules/AppPasswordInput'
-
 
 export default function App() {
   const { colorScheme } = useColorScheme()
@@ -44,8 +41,9 @@ export default function App() {
   if (!fontsLoaded || fontError) {
     return null
   }
+  const [modalVisible, setModalVisible] = useState(false)
 
-  const SHOW_STORYBOOK = true
+  const SHOW_STORYBOOK = false
   if (SHOW_STORYBOOK) {
     return <StorybookUIRoot />
   }
@@ -67,6 +65,14 @@ export default function App() {
     Alert.alert('Right icon 3 pressed')
   }
 
+  const handleOpenModal = () => {
+    setModalVisible(true)
+  }
+
+  const handleCloseModal = () => {
+    setModalVisible(false)
+  }
+
   return (
     <View className={'justify-center flex-1 bg-light-optiblue4 dark:bg-dark-optiblue4 px-2xl'}>
       <AppTopBar
@@ -83,7 +89,10 @@ export default function App() {
         onRightIconPress3={handleRightIconPress3}
       />
 
-      <AppModalLoader visible={false} />
+      <View className="flex-1 justify-center items-center">
+        <Button title="Show Loader" onPress={handleOpenModal} />
+        <AppModalLoader visible={modalVisible} />
+      </View>
       <AppText
         size={2}
         color={'text-dark-red9'}
@@ -102,49 +111,45 @@ export default function App() {
         spacing={1}
         titlePosition="top"
       />
-      
+
       <View>
         {[
           { label: 'Enter Email', key: 'email' },
           { label: 'Enter name', key: 'name' },
         ].map(item => (
           <>
-          <View
-            className='py-sm'
-          >
-            <Controller
-              rules={{
-                required: {
-                  value: true,
-                  message: `${item?.key} is compatible with the format `,
-                },
-              }}
-              control={control}
-              key={item.key}
-              render={({ field: { onChange, onBlur, value } }) => (
-                
-                
-//                 <AppInput
-//                     value={value}
-//                   label={item?.label}
-//                   autoFocus={false}
-//                   onBlur={onBlur}
-//                   onChangeText={onChange}
-//                   error={`${item?.key}  compatible with the format on our system`}/>
-                
-                <AppPasswordInput
-                  value={value}
-                  label={item?.label}
-                  autoFocus={false}
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  error={`${item?.key}  compatible with the format on our system`}
-                />
-              )}
-              name={'email'}
-            />
+            <View className="py-sm">
+              <Controller
+                rules={{
+                  required: {
+                    value: true,
+                    message: `${item?.key} is compatible with the format `,
+                  },
+                }}
+                control={control}
+                key={item.key}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  //                 <AppInput
+                  //                     value={value}
+                  //                   label={item?.label}
+                  //                   autoFocus={false}
+                  //                   onBlur={onBlur}
+                  //                   onChangeText={onChange}
+                  //                   error={`${item?.key}  compatible with the format on our system`}/>
+
+                  <AppPasswordInput
+                    value={value}
+                    label={item?.label}
+                    autoFocus={false}
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    error={`${item?.key}  compatible with the format on our system`}
+                  />
+                )}
+                name={'email'}
+              />
+            </View>
           </>
-          </View>
         ))}
       </View>
       <StatusBar style="dark" backgroundColor="red" />
