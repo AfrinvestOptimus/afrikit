44667a28ba6779f4fad65ecdf612b37916139950
Olashile-A <adegbiteolashile@ymail.com>
2024-09-05 17:06:27 +0100

    PROJ-2607: Icon component stories

---
+++ packages/mobile/.storybook/stories/AppIcon/AppIcon.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { View } from 'react-native'
+import IconTemp from '../../../molecules/IconTemp'
+
+const IconMeta: Meta<typeof IconTemp> = {
+  title: 'AppIconTemp',
+  component: IconTemp,
+  argTypes: {
+    name: {
+      control: 'text',
+    },
+    color: {
+      control: 'text',
+    },
+    size: {
+      conreol: 'select',
+      options: ['16', '20', '24', '40', '48'],
+    },
+  },
+  decorators: [
+    Story => (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Story />
+      </View>
+    ),
+  ],
+}
+
+export default IconMeta
+
+type Story = StoryObj<typeof IconMeta>
+
+export const Basic: Story = {
+  args: {
+    name: 'circle',
+    color: '',
+    size: '16',
+  },
+}
