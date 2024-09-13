8fb1852454e14479b95a3422e8e1f115b31a12c1
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-09-13 11:07:14 +0100

    PROJ-2487: update AppInput

--- packages/mobile/molecules/AppInput.tsx
+++ packages/mobile/molecules/AppInput.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-import { useColorScheme } from 'nativewind';
-import * as React from 'react';
+import { useColorScheme } from 'nativewind'
+import * as React from 'react'
 import {
   Animated,
   NativeSyntheticEvent,
@@ -9,18 +9,38 @@ import {
   TextInputFocusEventData,
   TouchableOpacity,
   TouchableWithoutFeedback,
-  View
-} from 'react-native';
-import { useSharedValue } from 'react-native-reanimated';
-import RemixIcon from 'react-native-remix-icon';
-import colors from '../../shared/colors';
-import { AppInputProps } from '../types/atoms';
-import { AppInputBlur } from '../utilities/validation';
-import AppHintText from './AppHintText';
+  View,
+} from 'react-native'
+import { useSharedValue } from 'react-native-reanimated'
+import RemixIcon from 'react-native-remix-icon'
+import colors from '../../shared/colors'
+import { AppInputProps } from '../types/atoms'
+import { AppInputBlur } from '../utilities/validation'
+import AppHintText from './AppHintText'
 
+const AppInput: React.FC<AppInputProps> = ({
+  onBlur,
+  FloatingLabel = true,
+  onFocus,
+  error,
+  multiline,
+  type,
+  label,
+  numberOfLines,
+  leftIcon,
+  rightIcon,
+  onChangeText: onChangeTextProp,
+  value = '',
+  containerStyle,
+  ...props
+}) => {
+  const textInputRef = React.useRef<TextInput>(null)
+  const [localValue, setLocalValue] = React.useState<string>('')
+  const [focused, setFocused] = React.useState<boolean>(false)
+  const [inputValue, setInputValue] = React.useState<string>(value)
+  const animatedIsFocused = React.useRef(new Animated.Value(value ? 1 : 0)).current
 
-const AppInput: React.FC<AppInputProps> =
-  ({
+  const AppInput: React.FC<AppInputProps> = ({
     onBlur,
     FloatingLabel = true,
     onFocus,
@@ -32,60 +52,62 @@ const AppInput: React.FC<AppInputProps> =
     onChangeText: onChangeTextProp,
     value = '',
     containerStyle,
-    ...props }) => {
-    const textInputRef = React.useRef<TextInput>(null);
-    const [localValue, setLocalValue] = React.useState<string>('');
-    const [focused, setFocused] = React.useState<boolean>(false);
-    const [inputValue, setInputValue] = React.useState<string>(value);
-    const animatedIsFocused = React.useRef(new Animated.Value(value ? 1 : 0)).current;
+    ...props
+  }) => {
+    const textInputRef = React.useRef<TextInput>(null)
+    const [localValue, setLocalValue] = React.useState<string>('')
+    const [focused, setFocused] = React.useState<boolean>(false)
+    const [inputValue, setInputValue] = React.useState<string>(value)
+    const animatedIsFocused = React.useRef(new Animated.Value(value ? 1 : 0)).current
     const { colorScheme } = useColorScheme()
     const isDarkMode = colorScheme === 'dark'
-
-    React.useEffect(() => {
-      Animated.timing(animatedIsFocused, {
-        toValue: focused || inputValue ? 1 : 0,
-        duration: 200,
-        useNativeDriver: false,
-      }).start();
-    }, [focused, animatedIsFocused, inputValue]);
-
-    // store init value 
-    const _text = useSharedValue(value);
 
     // handle input field change state at every steo
     const onChangeText = React.useCallback(
       (text: string) => {
-        setInputValue(text);
-        onChangeTextProp?.(text);
-        _text.value = text;
+        setInputValue(text)
+        onChangeTextProp?.(text)
+        _text.value = text
       },
-      [_text]
-    );
+      [_text],
+    )
+
+    // store init value
+    const _text = useSharedValue(value)
+
+    // handle input field change state at every steo
+    const onChangeText = React.useCallback(
+      (text: string) => {
+        setInputValue(text)
+        onChangeTextProp?.(text)
+        _text.value = text
+      },
+      [_text],
+    )
 
     // handle input not focused state
     const handleBlur = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
       if (type) {
-        AppInputBlur(localValue, onChangeText, false);
+        AppInputBlur(localValue, onChangeText, false)
       }
-      onBlur?.(e);
-      setFocused(false);
-    };
+      onBlur?.(e)
+      setFocused(false)
+    }
 
     // handle focus state for input field
     const handleFocus = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
-      onFocus?.(e);
-      setFocused(true);
-    };
+      onFocus?.(e)
+      setFocused(true)
+    }
 
-
-    // collate all props 
+    // collate all props
     const addedProps = {
       ...props,
       onChangeText: onChangeText,
       onBlur: handleBlur,
       onFocus: handleFocus,
       placeholder: FloatingLabel ? undefined : props.placeholder,
-    };
+    }
 
     const floatLabelStyle = {
       top: animatedIsFocused.interpolate({
@@ -100,40 +122,39 @@ const AppInput: React.FC<AppInputProps> =
         inputRange: [0, 1],
         outputRange: [12, 20],
       }),
-    };
+    }
     // handel clear input field
     const handleClear = () => {
-      setInputValue('');
-    };
-
+      setInputValue('')
+    }
 
     const getBorderStyle = () => {
       if (focused) {
         return error
           ? 'border-b-2 border-light-red9 dark:border-dark-red9 rounded-t-md' // show red bottom border when the input isn't valued
-          : 'border-b-2 border-light-blue10 dark:border-dark-blue10 rounded-t-md'; // show blue bottom border when the input is valid 
+          : 'border-b-2 border-light-blue10 dark:border-dark-blue10 rounded-t-md' // show blue bottom border when the input is valid
       }
       return error
         ? 'border-2 border-light-red9 dark:border-dark-red9 rounded-md' // show red border when the input isn't valued and the field isn't in a focused state
-        : 'border-0 border-transparent rounded-md'; // defaulting to rounded input when there's neither a focused state or error state on the input field
-    };
+        : 'border-0 border-transparent rounded-md' // defaulting to rounded input when there's neither a focused state or error state on the input field
+    }
 
     return (
       <View>
         <TouchableWithoutFeedback
           onPress={() => {
-            textInputRef.current?.focus();
+            textInputRef.current?.focus()
             //TODO: to be motified
             // props?.onPress?.();
-          }}
-        >
+          }}>
           <View
-            className={`px-sm items-center w-full flex-row bg-light-surface-gray dark:bg-dark-surface-gray h-[56px] ${getBorderStyle()} ${containerStyle}`}
-          >
-            <View
-              className="flex-1 px-xs ">
+            className={`px-sm items-center w-full flex-row bg-light-surface-gray dark:bg-dark-surface-gray h-[56px] ${getBorderStyle()} ${containerStyle}`}>
+            <View className="flex-1 px-xs ">
               {FloatingLabel && (
-                <Animated.Text style={[floatLabelStyle]} className="gap-xs  dark:text-dark-type-gray-muted text-light-type-gray-muted text-sm-title" allowFontScaling={false}>
+                <Animated.Text
+                  style={[floatLabelStyle]}
+                  className="gap-xs  dark:text-dark-type-gray-muted text-light-type-gray-muted text-sm-title"
+                  allowFontScaling={false}>
                   {label}
                 </Animated.Text>
               )}
@@ -146,30 +167,125 @@ const AppInput: React.FC<AppInputProps> =
                 autoFocus={focused}
                 numberOfLines={numberOfLines}
                 value={inputValue}
-                selectionColor='text-light-type-gray'
-                className='text-light-type-gray dark:text-dark-type-gray'
+                selectionColor="text-light-type-gray"
+                className="text-light-type-gray dark:text-dark-type-gray"
               />
             </View>
             {inputValue.length > 0 && (
-              <TouchableOpacity onPress={handleClear} className='justify-center pl-2'>
-                <RemixIcon name="close-circle-fill" size={24}
-                  color={
-                    colors[isDarkMode ? 'dark' : 'light'].type.gray.DEFAULT
-                  } />
+              <TouchableOpacity onPress={handleClear} className="justify-center pl-2">
+                <RemixIcon
+                  name="close-circle-fill"
+                  size={24}
+                  color={colors[isDarkMode ? 'dark' : 'light'].type.gray.DEFAULT}
+                />
               </TouchableOpacity>
             )}
           </View>
         </TouchableWithoutFeedback>
         {!!error &&
           (typeof error === 'string' ? (
-            <AppHintText
-              type="error"
-              text={error}
-              className='py-xs'
-            />
-          ) : undefined)
-        }
+            <AppHintText type="error" text={error} className="py-xs" />
+          ) : undefined)}
       </View>
     )
   }
+
+  // handle focus state for input field
+  const handleFocus = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    onFocus?.(e)
+    setFocused(true)
+  }
+
+  // collate all props
+  const addedProps = {
+    ...props,
+    onChangeText: onChangeText,
+    onBlur: handleBlur,
+    onFocus: handleFocus,
+    placeholder: FloatingLabel ? undefined : props.placeholder,
+  }
+
+  const floatLabelStyle = {
+    top: animatedIsFocused.interpolate({
+      inputRange: [0, 1],
+      outputRange: [10, 0],
+    }),
+    fontSize: animatedIsFocused.interpolate({
+      inputRange: [0, 1],
+      outputRange: [12, 11],
+    }),
+    lineHeight: animatedIsFocused.interpolate({
+      inputRange: [0, 1],
+      outputRange: [12, 20],
+    }),
+  }
+  // handel clear input field
+  const handleClear = () => {
+    setInputValue('')
+  }
+
+  const getBorderStyle = () => {
+    if (focused) {
+      return error
+        ? 'border-b-2 border-light-red9 rounded-t-xs' // show red bottom border when the input isn't valued
+        : 'border-b-2 border-light-blue10 rounded-t-xs' // show blue bottom border when the input is valid
+    }
+    return error
+      ? 'border-2 border-light-red9 rounded-xs' // show red border when the input isn't valued and the field isn't in a focused state
+      : 'border-0 border-transparent rounded-xs' // defaulting to rounded input when there's neither a focused state or error state on the input field
+  }
+
+  return (
+    <View>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          //TODO: to be motified
+          // props?.onPress?.();
+        }}>
+        <View
+          className={`px-sm items-center w-full flex-row bg-light-surface-gray h-[56px] ${getBorderStyle()} ${containerStyle}`}>
+          {leftIcon && (
+            <View className="items-center justify-center h-full ml-auto px-xs">{leftIcon}</View>
+          )}
+          <View className="flex-1">
+            {FloatingLabel && (
+              <Animated.Text
+                style={[floatLabelStyle]}
+                className="gap-xs color-dark-gray1"
+                allowFontScaling={false}>
+                {label}
+              </Animated.Text>
+            )}
+
+            <TextInput
+              {...addedProps}
+              ref={textInputRef}
+              allowFontScaling={false}
+              hitSlop={{ top: 10, bottom: 10, left: 40, right: 40 }}
+              returnKeyType="done"
+              autoFocus={focused}
+              numberOfLines={numberOfLines}
+              value={inputValue}
+              multiline={multiline}
+            />
+          </View>
+          {rightIcon && (
+            <View className="items-center justify-center h-full ml-auto px-xs">{rightIcon}</View>
+          )}
+          {inputValue.length > 0 && (
+            <TouchableOpacity onPress={handleClear} className="justify-center pl-2">
+              <RemixIcon name="close-circle-fill" size={24} color={colors.dark['white-to-dark']} />
+            </TouchableOpacity>
+          )}
+        </View>
+      </TouchableWithoutFeedback>
+      {!!error &&
+        (typeof error === 'string' ? (
+          <AppText size={2} color="error" weight={'regular'} align={'left'} className={'mb-5xl'}>
+            {error}
+          </AppText>
+        ) : undefined)}
+    </View>
+  )
+}
 export default AppInput
