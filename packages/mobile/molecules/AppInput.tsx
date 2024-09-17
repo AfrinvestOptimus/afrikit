366edc2a0c2d0bb8c9ff783f9eca2161f0f14c5b
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-09-17 10:10:05 +0100

    chore: update AppInput

--- packages/mobile/molecules/AppInput.tsx
+++ packages/mobile/molecules/AppInput.tsx
@@ -18,27 +18,25 @@ import { AppInputProps } from '../types/atoms'
 import { AppInputBlur } from '../utilities/validation'
 import AppHintText from './AppHintText'
 
-const AppInput: React.FC<AppInputProps> = ({
-  onBlur,
-  FloatingLabel = true,
-  onFocus,
-  error,
-  multiline,
-  type,
-  label,
-  numberOfLines,
-  leftIcon,
-  rightIcon,
-  onChangeText: onChangeTextProp,
-  value = '',
-  containerStyle,
-  ...props
+export type AuthInputProps = {
+  count: number
+  isError?: boolean
+  errorMessage?: string
+  onValueChange?: (value: string) => void
+  isLoading?: boolean
+  keypad?: 'Custom' | 'Native'
+}
+
+const AuthInput: FC<AuthInputProps> = ({
+  count,
+  isError,
+  errorMessage,
+  onValueChange,
+  isLoading,
+  keypad,
 }) => {
-  const textInputRef = React.useRef<TextInput>(null)
-  const [localValue, setLocalValue] = React.useState<string>('')
-  const [focused, setFocused] = React.useState<boolean>(false)
-  const [inputValue, setInputValue] = React.useState<string>(value)
-  const animatedIsFocused = React.useRef(new Animated.Value(value ? 1 : 0)).current
+  const [value, setValue] = useState('')
+  const [codeKeys, setCodeKeys] = useState<string[] | undefined>(undefined)
 
   const AppInput: React.FC<AppInputProps> = ({
     onBlur,
@@ -190,102 +188,55 @@ const AppInput: React.FC<AppInputProps> = ({
     )
   }
 
-  // handle focus state for input field
-  const handleFocus = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
-    onFocus?.(e)
-    setFocused(true)
-  }
+  const styles = getDesignClasses()
 
-  // collate all props
-  const addedProps = {
-    ...props,
-    onChangeText: onChangeText,
-    onBlur: handleBlur,
-    onFocus: handleFocus,
-    placeholder: FloatingLabel ? undefined : props.placeholder,
-  }
-
-  const floatLabelStyle = {
-    top: animatedIsFocused.interpolate({
-      inputRange: [0, 1],
-      outputRange: [10, 0],
-    }),
-    fontSize: animatedIsFocused.interpolate({
-      inputRange: [0, 1],
-      outputRange: [12, 11],
-    }),
-    lineHeight: animatedIsFocused.interpolate({
-      inputRange: [0, 1],
-      outputRange: [12, 20],
-    }),
-  }
-  // handel clear input field
-  const handleClear = () => {
-    setInputValue('')
-  }
-
-  const getBorderStyle = () => {
-    if (focused) {
-      return error
-        ? 'border-b-2 border-light-red9 rounded-t-xs' // show red bottom border when the input isn't valued
-        : 'border-b-2 border-light-blue10 rounded-t-xs' // show blue bottom border when the input is valid
+  const handlePress = () => {
+    if (keypad === 'Native') {
+      KeyboardRef?.current?.focus()
     }
-    return error
-      ? 'border-2 border-light-red9 rounded-xs' // show red border when the input isn't valued and the field isn't in a focused state
-      : 'border-0 border-transparent rounded-xs' // defaulting to rounded input when there's neither a focused state or error state on the input field
   }
 
   return (
-    <View>
-      <TouchableWithoutFeedback
-        onPress={() => {
-          //TODO: to be motified
-          // props?.onPress?.();
-        }}>
-        <View
-          className={`px-sm items-center w-full flex-row bg-light-surface-gray h-[56px] ${getBorderStyle()} ${containerStyle}`}>
-          {leftIcon && (
-            <View className="items-center justify-center h-full ml-auto px-xs">{leftIcon}</View>
-          )}
-          <View className="flex-1">
-            {FloatingLabel && (
-              <Animated.Text
-                style={[floatLabelStyle]}
-                className="gap-xs color-dark-gray1"
-                allowFontScaling={false}>
-                {label}
-              </Animated.Text>
-            )}
-
-            <TextInput
-              {...addedProps}
-              ref={textInputRef}
-              allowFontScaling={false}
-              hitSlop={{ top: 10, bottom: 10, left: 40, right: 40 }}
-              returnKeyType="done"
-              autoFocus={focused}
-              numberOfLines={numberOfLines}
-              value={inputValue}
-              multiline={multiline}
-            />
+    <>
+      {isLoading ? (
+        <>
+          <View className="animate-spin w-[40] h-[40] border-4 rounded-full border-b-[#BDC8FF17] bg-gra">
+            <Text>isLoading</Text>
           </View>
-          {rightIcon && (
-            <View className="items-center justify-center h-full ml-auto px-xs">{rightIcon}</View>
+        </>
+      ) : (
+        <>
+          <TextInput className="hidden" onChangeText={onChange} ref={KeyboardRef} />
+          <Pressable className="flex justify-center" onPress={handlePress}>
+            <View
+              className={`flex items-center justify-between w-fit h-[40] flex-row p-lg rounded-xs-max ${isError ? `${styles.backgroundError}` : `${styles.backgroundDefault}`}`}>
+              {codeKeys?.map((row, rowIndex) => (
+                <View
+                  key={`${rowIndex + 1}`}
+                  className={classNames(
+                    `w-sm h-sm rounded-sm-max ${rowIndex == codeKeys.length - 1 ? 'mr-none' : 'mr-md'}`,
+                    `${row !== '' ? `${styles.pinBackgroundFilled}` : isError ? `${styles.pinBackgroundError}` : `${styles.pinBackgroundDefault}`}`,
+                  )}></View>
+              ))}
+            </View>
+          </Pressable>
+          {isError && (
+            <Text className="text-light-type-error dark:text-dark-type-error text-body2 font-semibold mt-md">
+              {errorMessage}
+            </Text>
           )}
-          {inputValue.length > 0 && (
-            <TouchableOpacity onPress={handleClear} className="justify-center pl-2">
-              <RemixIcon name="close-circle-fill" size={24} color={colors.dark['white-to-dark']} />
-            </TouchableOpacity>
-          )}
-        </View>
-      </TouchableWithoutFeedback>
-      {!!error &&
-        (typeof error === 'string' ? (
-          <AppText size={2} color="error" weight={'regular'} align={'left'} className={'mb-5xl'}>
-            {error}
-          </AppText>
-        ) : undefined)}
-    </View>
+        </>
+      )}
+    </>
   )
 }
-export default AppInput
+
+export default AuthInput
+
+const getDesignClasses = () => ({
+  backgroundDefault: 'bg-light-surface-gray dark:bg-dark-surface-gray',
+  backgroundError: 'bg-light-background-error-light dark:bg-dark-background-error-light',
+  pinBackgroundDefault: 'bg-light-neutral8 dark:bg-dark-neutral8',
+  pinBackgroundFilled: 'bg-light-background-accent-base bg-dark-background-accent-base',
+  pinBackgroundError: 'bg-light-type-error bg-dark-type-error',
+})
