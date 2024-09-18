f4c97fdd9c53248a653493129f3c5ecbc97f4af7
Mukhtar Oyelayo <mukhtaroyelayo@gmail.com>
2024-09-18 13:57:38 +0100

    PROJ-2574: Fix type issues on AppBottomSheet

--- packages/mobile/types/molecules.d.ts
+++ packages/mobile/types/molecules.d.ts
@@ -23,12 +23,13 @@ export type AppBottomSheetProps<T extends boolean> = CommonProps & {
         isSwipeable?: never
         secondaryActionButton?: ActionButtonProps
       } & ({ index: 0; height?: never } | { index?: never; height: number })
-    : {
+      : {
         title?: {
           text: string
           align?: 'center' | 'left'
           subtitle?: string
         }
+        content?: string
         isSwipeable?: boolean
         children: ReactNode
       } & ({ index: 0 | 1 | 2 | 3 | 4 | 5; height?: never } | { index?: never; height: number }))
