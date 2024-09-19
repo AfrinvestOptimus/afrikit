405618d0626d6f81933c3f1ad81ffc8b7a95bbf3
infinitypaul <infinitypaul@live.com>
2024-09-19 01:48:17 +0100

    Refactor mobile package exports and tsconfig paths
    Updated `index.ts` to export molecules and atoms, streamlining module access. Adjusted `tsconfig.json` paths and includes for better structure and alignment with current directory layout.

--- packages/mobile/index.ts
+++ packages/mobile/index.ts
@@ -1,3 +1,3 @@
-export * from './components/molecules'
-export * from './atoms'
-export * from './molecules'
+export * from './components/molecules';
+export * from './molecules';
+export * from './atoms';
