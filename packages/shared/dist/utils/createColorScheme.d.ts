7f15343e64514b2df9adc85f0a1ef582cbacb915
infinitypaul <infinitypaul@live.com>
2024-09-19 22:34:22 +0100

    Remove colors.d.ts file
    Deleted the colors.d.ts file from shared package as it was no longer needed. This change helps reduce unnecessary weight in the codebase and simplifies the maintenance of type definitions.

--- packages/shared/dist/utils/createColorScheme.d.ts
+++
@@ -1,6 +0,0 @@
-declare const createColorScheme: (colorSource: {
-    [key: string]: string;
-}, prefix: string | undefined, sourcePrefix: string) => {
-    [key: string]: string;
-};
-export default createColorScheme;
