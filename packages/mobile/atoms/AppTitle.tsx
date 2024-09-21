382c909291d734771edbf56b11f134b6dd727389
Amire Steve <zoe_steve@hotmail.com>
2024-09-21 06:18:00 +0100

    Enhance AppTitle component in mobile package
    Extended AppTitle component to include title alignment, position, spacing options, and a subtitle toggle. This provides greater flexibility and customization in rendering the AppTitle.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/mobile/atoms/AppTitle.tsx
+++ packages/mobile/atoms/AppTitle.tsx
@@ -4,14 +4,27 @@ import { Text, View } from 'react-native'
 import { AppTitleAtomProps } from '../types/atoms'
 import classNames from '../utilities/classnames'
 
-function AppTitle({ title, subtitle }: AppTitleAtomProps) {
+function AppTitle({
+  title,
+  subtitle,
+  align,
+  titlePosition = 'top',
+  spacing,
+  hasSubtitle = false,
+}: AppTitleAtomProps) {
   return (
-    <View className="items-center" style={{ borderWidth: 1, borderColor: 'red' }}>
-      <Text className="text-light-type-gray dark:text-dark-type-gray text-2xl font-bold">
+    <View
+      className={classNames(
+        '',
+        align === 'center' ? 'items-center' : 'items-start',
+        titlePosition === 'top' ? 'flex-col' : titlePosition === 'bottom' ? 'flex-col-reverse' : '',
+        spacing === 2 ? 'gap-y-sm' : spacing === 3 ? 'gap-y-lg' : '',
+      )}>
+      <Text className={classNames('text-light-type-gray dark:text-dark-type-gray text-2xl-bold')}>
         {title}
       </Text>
-      {subtitle && (
-        <Text className="text-light-type-gray-muted dark:text-dark-type-gray-muted text-sm">
+      {hasSubtitle && (
+        <Text className={'text-light-type-gray-muted dark:text-dark-type-gray-muted text-sm-body'}>
           {subtitle}
         </Text>
       )}
