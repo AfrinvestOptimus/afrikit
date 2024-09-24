cf18d7d735559bd1a493f6f4a946c50ccd497fae
Amire Steve <zoe_steve@hotmail.com>
2024-09-24 10:47:58 +0100

    Switch to TailwindCSS and remove old CSS files
    This commit replaces the traditional CSS setup with TailwindCSS, removing App.css and index.css. It also introduces TailwindCSS configuration and adds necessary dependencies in package.json, such as autoprefixer and postcss. Ensure the updated styling configurations align with the new TailwindCSS setup.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useTransition } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,7 +22,7 @@ function App() {
   }
 
   return (
-    <>
+    <div className={'bg-light-optiblue11'}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -40,7 +39,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   )
 }
 
---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import afrikitConfig from 'afrikit-shared/dist'
+export default {
+  content: [
+    './src/**/*.{js,ts,tsx,jsx}',
+    './.storybook/**/*.{js,ts,tsx,jsx}',
+    './atoms/**/*.{js,ts,tsx,jsx}',
+    './molecules/**/*.{js,ts,tsx,jsx}',
+    './organisms/**/*.{js,ts,tsx,jsx}',
+    './templates/**/*.{js,ts,tsx,jsx}',
+  ],
+  presets: [afrikitConfig],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
