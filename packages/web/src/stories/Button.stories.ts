cb21073c786f58ecefc75ac36933b45e0cee9d3a
Amire Steve <zoe_steve@hotmail.com>
2024-09-26 22:45:20 +0100

    Update TypeScript configs and React story typing
    Added comments for clarity in tsconfig.json and detailed Meta typing in Button stories. Enhanced project understanding and maintainability with these changes.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/web/src/stories/Button.stories.ts
+++ packages/web/src/stories/Button.stories.ts
@@ -4,7 +4,7 @@ import { fn } from '@storybook/test'
 import { Button } from './Button'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
@@ -19,9 +19,10 @@ const meta = {
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>
+}
 
 export default meta
+
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
