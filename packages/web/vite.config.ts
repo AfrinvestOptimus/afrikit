cfcc8f7b6089e750a0ad8b29fd57a918512028da
Amire Steve <zoe_steve@hotmail.com>
2024-09-26 22:21:06 +0100

    Update TypeScript and React configurations
    Migrated to ESNext modules, added strict typing, and modified paths in tsconfig.json. Standardized React import statements and adjusted asset paths. Enhanced Vite config with path aliases for improved module resolution.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,18 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
