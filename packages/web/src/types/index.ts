2a4539ac1c6e31970d083ee410b52b90377ccbda
Amire Steve <zoe_steve@hotmail.com>
2024-09-27 12:33:03 +0100

    Refactor: Restructure directories and update imports
    PROJ-2704-Implement-Sidebar-component
    Moved components into a structured directory hierarchy and added TypeScript type exports for cleaner code organization. Also updated `tsconfig.json` with path aliasing for easier imports and refined Tailwind configuration to focus on necessary files.
    
    Signed-off-by: Amire Steve <zoe_steve@hotmail.com>

--- packages/web/src/types/index.ts
+++ packages/web/src/types/index.ts
@@ -1,8 +1,2 @@
 // Exports out all types used in the project
-export type { AppButtonProps } from './TAppButton'
-<<<<<<< HEAD
-export type { DropdownListItemProps } from './TAppDropdownMenu'
-=======
->>>>>>> 37cf46d (PROJ-PROJ-2710 Implement Dropdown menu component)
 export type { AppInputProps } from './TAppInput'
-export type { AppDropdownListItem } from './TAppDropdownMenu'
