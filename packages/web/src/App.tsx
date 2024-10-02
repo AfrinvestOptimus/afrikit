30cfff003929d25552f960c12f5e9f19d24e02c3
Amire Steve <zoe_steve@hotmail.com>
2024-10-02 19:50:46 +0100

    feat: Created the Topbar Component - PROJ-2706

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -5,30 +5,13 @@ import AppInput from './components/molecules/AppInput'
 import 'remixicon/fonts/remixicon.css'
 import AppButton from './components/molecules/AppButton'
 import DropdownMenu from './components/molecules/AppDropdownMenu'
+import { AppTopBar } from './molecules/AppTopBar'
 
 interface FormData {
   email: string
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [isPending, startTransition] = useTransition()
-
-  const handleClick = () => {
-    // Urgent Update
-    setCount(count => count + 1)
-
-    // Start a non-urgent update
-    startTransition(() => {
-      // Simulating a heavy computation
-      const startTime = Date.now()
-      while (Date.now() - startTime < 100) {
-        // Artificial delay
-      }
-      setCount(count => count + 1)
-    })
-  }
-
   const {
     control,
     handleSubmit,
@@ -45,30 +28,13 @@ function App() {
     setValue('email', '') // Clear the email value
   }
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <form className="max-w-md mx-auto">
-        <Controller
-          name="email"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <AppInput
-              label="Testing"
-              type="email"
-              // placeholder="meeeee"
-              {...field} // Pass field props which includes value and onChange
-              // error={errors.email?.message}
-              onClear={handleClearEmail}
-            />
-          )}
-        />
-      </form>
-
-      <AppButton text="Continue" color="accent" />
-      <DropdownMenu />
-      <div>
-        <AppButton text="Continue" color="accent" />
-      </div>
+    <div className="flex justify-center items-center min-h-screen bg-light-page-bg2 dark:bg-dark-page-bg2 font-sans">
+      <AppTopBar
+        theme="ghost"
+        isOnboarding={false}
+        pageTitle="Page title"
+        subtitle="Find all your payment transactions here"
+      />
     </div>
   )
 }
