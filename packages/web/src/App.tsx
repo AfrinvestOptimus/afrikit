cc74c77413f65c8031efa04085bcce0036107e20
Amire Steve <zoe_steve@hotmail.com>
2024-10-04 12:45:44 +0100

    refactor: cleaned up app.tsx

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
     setValue('email', '') // Clear the email value
   }
   return (
-    <div className="flex justify-center items-center min-h-screen bg-light-page-bg2 dark:bg-dark-page-bg2 font-sans antialiased">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-light-page-bg2 dark:bg-dark-page-bg2 font-sans antialiased">
       <AppTopBar
         theme="filled"
         isOnboarding={false}
@@ -40,40 +40,41 @@ function App() {
         buttonOne={false}
         backBtn
       />
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <form className="max-w-md mx-auto">
-        <Controller
-          name="email"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <AppPhoneInput
-              label="Testing"
-              // placeholder="meeeee"
-              onInput={e => {
-                // Cast EventTarget to HTMLInputElement
-                const input = e.target as HTMLInputElement
-                // Prevent non-numeric input
-                input.value = input.value.replace(/[^0-9]/g, '')
-                field.onChange(input.value) // Update the form value
-              }}
-              {...field} // Pass field props which includes value and onChange
-              // error={errors.email?.message}
-              onClear={handleClearEmail}
-            />
-          )}
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <form className="max-w-md mx-auto">
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <AppPhoneInput
+                label="Testing"
+                // placeholder="meeeee"
+                onInput={e => {
+                  // Cast EventTarget to HTMLInputElement
+                  const input = e.target as HTMLInputElement
+                  // Prevent non-numeric input
+                  input.value = input.value.replace(/[^0-9]/g, '')
+                  field.onChange(input.value) // Update the form value
+                }}
+                {...field} // Pass field props which includes value and onChange
+                // error={errors.email?.message}
+                onClear={handleClearEmail}
+              />
+            )}
+          />
+        </form>
+
+        <AppCodeInput
+          length={6}
+          errorMessage=""
+          secureEntry
+          // onChange={handleCodeChange}
         />
-      </form>
 
-      <AppCodeInput
-        length={6}
-        errorMessage=""
-        secureEntry
-        // onChange={handleCodeChange}
-      />
-
-      <AppButton text="Continue" color="accent" />
-      <DropdownMenu />
+        <AppButton text="Continue" color="accent" />
+        <DropdownMenu />
+      </div>
     </div>
   )
 }
