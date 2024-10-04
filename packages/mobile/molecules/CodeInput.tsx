82782d46df6312ff6efa0aeee0e0fc0d497d416f
Olashile-A <adegbiteolashile@ymail.com>
2024-10-04 16:33:37 +0100

    update components

--- packages/mobile/molecules/CodeInput.tsx
+++ packages/mobile/molecules/CodeInput.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
+import React from 'react'
 import { useColorScheme } from 'nativewind'
 import { FC, useEffect, useRef, useState, type RefObject } from 'react'
 import { TextInput, View } from 'react-native'
