2bd51c054363b039552b72aeb25be690a2cf85ef
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-10-04 11:02:35 +0100

    PROJ-2561: update types for components

--- packages/mobile/types/atoms.ts
+++ packages/mobile/types/atoms.ts
@@ -27,7 +27,6 @@ export type AppTextAtomProps = AppTextOwnProps & Omit<TextProps, keyof AppTextOw
 export type AppTitleAtomProps = {
   title: string
   align: 'left' | 'center'
-  hasSubtitle: boolean
   subtitle?: string
   titlePosition?: 'top' | 'bottom'
   spacing?: 1 | 2 | 3
