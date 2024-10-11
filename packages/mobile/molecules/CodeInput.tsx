bde5f6d65aa44226c488f0d8b10e1cf4daefb2d2
kyeltech <kyel2030@gmail.com>
2024-10-11 10:36:01 +0100

    PROJ-Modify CodeInput

--- packages/mobile/molecules/CodeInput.tsx
+++ packages/mobile/molecules/CodeInput.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-import React from 'react'
 import { useColorScheme } from 'nativewind'
 import { FC, useEffect, useRef, useState, type RefObject } from 'react'
 import { TextInput, View } from 'react-native'
-import KeyPad from './keypad'
+// import KeyPad from './keypad'
 
 export interface OTPInputProps {
   count?: number
@@ -12,8 +11,10 @@ export interface OTPInputProps {
   isError?: boolean
   onFullCode?: (fullCode: string) => void
   keypad?: 'Custom' | 'Native'
-  KeyPadType: 'decimal' | 'nondecimal' | 'biometric'
+  keyPadType: 'decimal' | 'nondecimal' | 'biometric'
   containerClassName?: string
+  onKeyPress?: (fullCode: string) => void
+  codeValue?: string
 }
 
 const CodeInput: FC<OTPInputProps> = ({
@@ -21,9 +22,11 @@ const CodeInput: FC<OTPInputProps> = ({
   secureEntry,
   isError,
   onFullCode,
-  keypad,
-  KeyPadType = 'decimal',
+  // keypad,
+  // keyPadType = 'decimal',
   containerClassName,
+  onKeyPress,
+  codeValue = '',
 }) => {
   const [codes, setCodes] = useState<string[]>(Array(count).fill(''))
   const refs: RefObject<TextInput>[] = Array.from({ length: count }, () => useRef<TextInput>(null))
@@ -31,11 +34,21 @@ const CodeInput: FC<OTPInputProps> = ({
 
   useEffect(() => {
     refs[0]?.current?.focus()
-  }, [keypad])
+  }, [])
+
+  useEffect(() => {
+    const updatedCodes = codeValue.split('').slice(0, count)
+    setCodes(prevCodes => {
+      const newCodes = [...prevCodes]
+      updatedCodes.forEach((char, index) => {
+        newCodes[index] = char
+      })
+      return newCodes
+    })
+  }, [codeValue, count])
 
   const onChangeCode = (text: string, index: number) => {
     const newCodes = [...codes]
-
     if (text.length <= 1) {
       newCodes[index] = text
       setCodes(newCodes)
@@ -43,47 +56,10 @@ const CodeInput: FC<OTPInputProps> = ({
       if (text !== '' && index < count - 1) {
         refs[index + 1]!.current?.focus()
       }
-
       const fullCode = newCodes.join('')
       if (fullCode.length === count) {
         onFullCode?.(fullCode)
       }
-    }
-  }
-
-  const handleKeyPress = (key: string, index: number) => {
-    if (key === 'Backspace') {
-      if (codes[index] === '') {
-        if (index > 0) {
-          refs[index - 1]!.current!.focus()
-        }
-      } else {
-        onChangeCode('', index)
-        if (index > 0) {
-          refs[index - 1]!.current!.focus()
-        }
-      }
-    }
-  }
-
-  const handleKeyPressFromKeyPad = (key: string) => {
-    const nextEmptyIndex = codes.findIndex(code => code === '')
-
-    if (key === '←') {
-      const filledIndex =
-        codes.length -
-        1 -
-        codes
-          .slice()
-          .reverse()
-          .findIndex(code => code !== '')
-
-      if (filledIndex < count) {
-        onChangeCode('', filledIndex)
-        refs[filledIndex]!.current!.focus()
-      }
-    } else if (nextEmptyIndex !== -1) {
-      onChangeCode(key, nextEmptyIndex)
     }
   }
 
@@ -101,21 +77,16 @@ const CodeInput: FC<OTPInputProps> = ({
                 ? 'text-light-type-error dark:text-dark-type-error bg-light-background-error-light dark:bg-dark-background-error-light'
                 : 'text-light-type-gray dark:text-dark-type-gray bg-light-surface-gray dark:bg-dark-surface-gray'
             }`}
-            inputMode="numeric"
+            inputMode="none"
             onChangeText={(text: string) => onChangeCode(text, index)}
             value={code}
             maxLength={index === 0 ? codes.length : 1}
             ref={refs[index]}
-            onKeyPress={({ nativeEvent: { key } }) => handleKeyPress(key, index)}
+            editable={false}
+            onKeyPress={({ nativeEvent: { key } }) => onKeyPress?.(key)}
           />
         ))}
       </View>
-      <KeyPad
-        type={KeyPadType}
-        textLength={count}
-        onKeyPress={handleKeyPressFromKeyPad}
-        onDone={onFullCode}
-      />
     </View>
   )
 }
