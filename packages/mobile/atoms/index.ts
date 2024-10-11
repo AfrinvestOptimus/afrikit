63ea16569c2d67f9738df69fed2d3c4179721481
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2024-10-11 13:38:02 +0100

    PROJ-2561: export Loader

--- packages/mobile/atoms/index.ts
+++ packages/mobile/atoms/index.ts
@@ -1,4 +1,4 @@
 export type { AppInputProps, AppTextAtomProps } from '../types/atoms'
 export { default as AppText } from './AppText'
 export { default as AppTitle } from './AppTitle'
-
+export { default as Loader, type LoaderTypes } from './Loader'
