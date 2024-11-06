06e177daaec871c15222b7ce1d25b7e4c03b71ac
infinitypaul <infinitypaul@live.com>
2024-11-06 16:20:14 +0100

    Add asset copying step and clean up Metro config
    Added a new step to copy assets to the dist folder in the GitHub Actions workflow. Removed custom asset extensions and watch folders from the Metro configuration for a cleaner setup.

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 const { getDefaultConfig } = require('expo/metro-config')
 const { withNativeWind } = require('nativewind/metro')
-const path = require('path');
 // const { generate } = require('@storybook/react-native/scripts/generate');
 
 // generate({
@@ -10,19 +9,5 @@ const path = require('path');
 /* eslint-disable */
 
 const config = getDefaultConfig(__dirname)
-
-config.resolver.assetExts = [
-    ...config.resolver.assetExts,
-    'png',
-    'jpg',
-    'jpeg',
-    'gif',
-    'svg',
-    'webp',
-];
-
-config.watchFolders = [
-    path.resolve(__dirname, './assets'),
-];
 
 module.exports = withNativeWind(config, { input: './global.css' })
