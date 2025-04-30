520b333f9635822dd2fda517d54ad87b1383f553
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2025-04-30 14:32:02 +0100

    PROJ-3518: implement new product icons

--- packages/mobile/molecules/AppList/index.tsx
+++ packages/mobile/molecules/AppList/index.tsx
@@ -3,10 +3,15 @@ import { cssInterop, useColorScheme } from 'nativewind'
 import React from 'react'
 import { Image, Switch, Text, TouchableOpacity, View } from 'react-native'
 import RemixIcon from 'react-native-remix-icon'
+import ProductCommercialPaper from '../../assets/product-commercial-paper.png'
 import ProductEarn from '../../assets/product-earn.png'
-import ProductFlex from '../../assets/product-flex.png'
-import ProductLock from '../../assets/product-lock.png'
-import ProductTarget from '../../assets/product-target.png'
+import ProductFixedDeposit from '../../assets/product-fixed-deposit.png'
+import ProductMutualFunds from '../../assets/product-mutual-funds.png'
+import ProductFlex from '../../assets/product-optiflex.png'
+import ProductLock from '../../assets/product-optilock.png'
+import ProductTarget from '../../assets/product-optitarget.png'
+import ProductTreasuryBill from '../../assets/product-treasury-bill.png'
+import ProductUSStocks from '../../assets/product-us-stock.png'
 import AppText from '../../atoms/AppText'
 import { AppTextAtomProps } from '../../types/atoms'
 import {
@@ -111,6 +116,11 @@ const productIcons = {
   OptiLock: ProductLock,
   OptiTarget: ProductTarget,
   Earn: ProductEarn,
+  MutualFunds: ProductMutualFunds,
+  USStocks: ProductUSStocks,
+  FixedDeposit: ProductFixedDeposit,
+  TreasuryBill: ProductTreasuryBill,
+  CommercialPaper: ProductCommercialPaper,
 }
 
 const densitySpacing = {
