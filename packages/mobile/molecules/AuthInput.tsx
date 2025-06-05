02acd9efa53be3dc3f4a16f4cf9aecca024ca53e
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2025-06-05 21:40:19 +0100

    PROJ-3518: update AuthInput component

--- packages/mobile/molecules/AuthInput.tsx
+++ packages/mobile/molecules/AuthInput.tsx
@@ -143,12 +143,18 @@ const AuthInput = forwardRef<AuthInputRef, AuthInputProps>(
               </View>
             </Pressable>
             {isError ? (
-              <View className="flex-row items-center gap-sm justify-center">
-                <AppHintText text={errorMessage as string} type="error" />
-                <View className="w-xs h-xs rounded-full bg-light-background-neutral-transparent-pressed dark:bg-dark-background-neutral-transparent-pressed" />
-                <AppText size={2} weight="semibold" color="accent" onPress={onActionPress}>
-                  {actionLabel}
-                </AppText>
+              <View className="flex-col items-center justify-center mt-sm">
+                <View className="w-full">
+                  <AppHintText text={errorMessage as string} type="error" className="text-center" />
+                </View>
+                {actionLabel && (
+                  <View className="flex-row items-center gap-sm mt-xs">
+                    <View className="w-xs h-xs rounded-full bg-light-background-neutral-transparent-pressed dark:bg-dark-background-neutral-transparent-pressed" />
+                    <AppText size={2} weight="semibold" color="accent" onPress={onActionPress}>
+                      {actionLabel}
+                    </AppText>
+                  </View>
+                )}
               </View>
             ) : null}
           </>
